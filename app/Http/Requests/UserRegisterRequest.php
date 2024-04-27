6a1868578e0f12643ad79292e3717edf658f1a99
Zoltan Keresztes <zolikeresztes90@gmail.com>
2024-04-27 21:48:17 +0300

    add users table changes and custom send email verification

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'address' => ['string', 'max:255'],
+            'phone' => ['string', 'max:255'],
+            'avatar' => ['image', 'mimes:jpeg,jpg,png', 'max:2048'],
+        ];
+    }
+}
